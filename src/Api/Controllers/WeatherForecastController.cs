b403f2b4c86b373febc9ba6c2fdba6e01bbfc919
TopSwagCode <josh@topswagcode.com>
2021-09-04 22:07:49 +0200

    remove unused logger

--- src/Api/Controllers/WeatherForecastController.cs
+++ src/Api/Controllers/WeatherForecastController.cs
@@ -18,13 +18,6 @@ namespace Api.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
